c3dbea4c76b6b1ff49dc19460fd12c67a09c3000
assafavital <assaf@raftt.io>
2022-07-09 13:18:25 +0300

    modified Dockerfile and raftt.yml

--- dev/cmds.sh
+++ dev/cmds.sh
@@ -3,8 +3,10 @@
 
 deploy_backoffice() {
     cd /code || return
-    make GIT_TAG="dev" build/linux/backoffice || return 1
+    make GIT_TAG="dev" build/linux/backoffice
     raftt stop backoffice
-    raftt cp build/linux/backoffice backoffice:/app/build/linux/backoffice
+    raftt sh backoffice -- sh -c 'cat /dev/null > /tmp/log/raftt/lifeguard/hijacked.out'
+    raftt cp build/linux/backoffice backoffice:/code/build/linux/backoffice
     raftt restart backoffice
+    raftt logs backoffice -f | /root/.cargo/bin/fblog
 }
